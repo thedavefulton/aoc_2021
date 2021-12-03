48a1690ae777e72bcc1dd813a263a68b0a44af2e
David Fulton <thedavefulton@hey.com>
2021-12-02 20:14:22 -0600

    day_01 part 1 complete

--- day_02/src/__tests__/partOne.spec.ts
+++ day_02/src/__tests__/partOne.spec.ts
@@ -1,5 +1,18 @@
-// import { sum } from '../partOne';
-//
-// test('sum', () => {
-//   expect(sum()).toBe(0);
-// });
+import { processSteps, splitSteps } from '../partOne';
+
+export const mockSubSteps = ['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2'];
+
+test('splitSteps', () => {
+  expect(splitSteps(mockSubSteps)).toStrictEqual([
+    ['forward', '5'],
+    ['down', '5'],
+    ['forward', '8'],
+    ['up', '3'],
+    ['down', '8'],
+    ['forward', '2'],
+  ]);
+});
+
+test('processSteps', () => {
+  expect(processSteps(splitSteps(mockSubSteps))).toStrictEqual({ depth: 10, distance: 15 });
+});
--- day_02/src/partOne.ts
+++ day_02/src/partOne.ts
@@ -1,5 +1,31 @@
-import { shout } from './utils';
+import { readFile, shout } from './utils';
 
-// export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
+export function splitSteps(steps: string[]) {
+  return steps.map((step) => step.split(' '));
+}
 
-export const partOneShout = () => shout('Hello from Part One!');
+export function processSteps(splitSteps: string[][]) {
+  return splitSteps.reduce(
+    (acc, cur) => {
+      const [command, val] = cur;
+
+      if (command === 'forward') {
+        acc.distance += parseInt(val);
+      } else if (command === 'down') {
+        acc.depth += parseInt(val);
+      } else {
+        acc.depth -= parseInt(val);
+      }
+
+      return acc;
+    },
+    { depth: 0, distance: 0 }
+  );
+}
+
+export const partOneShout = async () => {
+  const lines = await readFile();
+  const { depth, distance } = processSteps(splitSteps(lines));
+
+  shout(depth * distance);
+};
