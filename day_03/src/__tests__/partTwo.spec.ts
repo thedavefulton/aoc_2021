b694485404f78acd7f568124338c76167165bc99
David Fulton <thedavefulton@hey.com>
2021-12-03 12:47:01 -0600

    day_03 part 2 complete

--- day_03/src/__tests__/partTwo.spec.ts
+++ day_03/src/__tests__/partTwo.spec.ts
@@ -1,5 +1,49 @@
-import { sum } from '../partTwo';
+import { mockReadings } from './partOne.spec';
+import { LifeSupport } from '../partTwo';
 
-test('sum', () => {
-  expect(sum()).toBe(0);
+test('loadReadings', async () => {
+  const lifeSupport = new LifeSupport(mockReadings);
+
+  expect(lifeSupport.readings).toStrictEqual(mockReadings);
+});
+
+test('filterMaxReadingsByColumnIndex', () => {
+  const lifeSupport = new LifeSupport(mockReadings);
+
+  lifeSupport.filterMaxReadingsByColumnIndex(0);
+
+  expect(lifeSupport.readings).toStrictEqual([
+    '11110',
+    '10110',
+    '10111',
+    '10101',
+    '11100',
+    '10000',
+    '11001',
+  ]);
+});
+test('filterMinReadingsByColumnIndex', () => {
+  const lifeSupport = new LifeSupport(mockReadings);
+
+  lifeSupport.filterMinReadingsByColumnIndex(0);
+
+  expect(lifeSupport.readings).toStrictEqual([
+    '00100',
+    '01111',
+    '00111',
+    '00010',
+    '01010',
+  ]);
+});
+
+test('get generatorRating', () => {
+  const lifeSupport = new LifeSupport(mockReadings);
+
+  expect(lifeSupport.generatorRating).toBe(23);
+});
+
+test('get scrubberRating', () => {
+  const lifeSupport = new LifeSupport(mockReadings);
+
+  expect(lifeSupport.scrubberRating).toBe(10);
 });
--- day_03/src/partTwo.ts
+++ day_03/src/partTwo.ts
@@ -1,5 +1,77 @@
-import { shout } from './utils';
+import { readFile, shout } from './utils';
 
-export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
+export class LifeSupport {
+  _readings: string[];
+  readings: string[];
 
-export const partTwoShout = () => shout('Hello from Part Two!');
+  constructor(readings: string[]) {
+    this._readings = readings;
+    this.readings = readings;
+  }
+
+  separateReadingsByColumnIndex(columnIndex: number) {
+    return this.readings.reduce(
+      (acc, cur) => {
+        if (cur[columnIndex] == '0') {
+          acc.zeros.push(cur);
+        } else {
+          acc.ones.push(cur);
+        }
+
+        return acc;
+      },
+      { zeros: [] as string[], ones: [] as string[] }
+    );
+  }
+
+  filterMaxReadingsByColumnIndex(columnIndex: number) {
+    const { zeros, ones } = this.separateReadingsByColumnIndex(columnIndex);
+    console.log({ zeros, ones });
+
+    this.readings = zeros.length > ones.length ? zeros : ones;
+  }
+
+  filterMinReadingsByColumnIndex(columnIndex: number) {
+    const { zeros, ones } = this.separateReadingsByColumnIndex(columnIndex);
+    console.log({ zeros, ones });
+
+    this.readings = ones.length < zeros.length ? ones : zeros;
+  }
+
+  get readingLength() {
+    return this._readings[0] || 0;
+  }
+
+  get generatorRating() {
+    this.readings = [...this._readings];
+
+    for (let i = 0; i < this.readingLength; i++) {
+      this.filterMaxReadingsByColumnIndex(i);
+
+      if (this.readings.length === 1) break;
+    }
+
+    return parseInt(this.readings[0], 2);
+  }
+
+  get scrubberRating() {
+    this.readings = [...this._readings];
+
+    for (let i = 0; i < this.readingLength; i++) {
+      this.filterMinReadingsByColumnIndex(i);
+
+      if (this.readings.length === 1) break;
+    }
+
+    return parseInt(this.readings[0], 2);
+  }
+
+  get lifeSupportRating() {
+    return this.generatorRating * this.scrubberRating;
+  }
+}
+
+export const partTwoShout = async () => {
+  const lines = await readFile();
+  shout(new LifeSupport(lines).lifeSupportRating);
+};
