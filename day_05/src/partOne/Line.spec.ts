8f5a43a2ed01358822cd092352cb07cb6cf39ea4
David Fulton <thedavefulton@hey.com>
2021-12-06 10:50:56 -0600

    day_05 added line points

--- day_05/src/partOne/Line.spec.ts
+++ day_05/src/partOne/Line.spec.ts
@@ -16,3 +16,12 @@ test('line correctly parses input', () => {
   expect(diagonalLine.isHorizontal).toBe(false);
   expect(diagonalLine.isVertical).toBe(false);
 });
+
+test('line.points is correctly generated', () => {
+  const line = new Line('0,0 -> 0,2');
+  expect(line.points[0].x).toBe(0);
+  expect(line.points[0].y).toBe(0);
+
+  expect(line.points[2].x).toBe(0);
+  expect(line.points[2].y).toBe(2);
+});
--- day_05/src/partOne/Line.ts
+++ day_05/src/partOne/Line.ts
@@ -1,28 +1,45 @@
 import { Point } from './Point';
+import { range } from '../utils';
 
 export class Line {
   // _lineString === 'x1,y1 -> x2,y2'
   private _lineString: string;
-  private _pointOne: Point;
-  private _pointTwo: Point;
+  readonly pointOne: Point;
+  readonly pointTwo: Point;
 
   constructor(lineString: string) {
     this._lineString = lineString;
 
     const [pointOne, pointTwo] = this.parseLineString();
-    this._pointOne = pointOne;
-    this._pointTwo = pointTwo;
+    this.pointOne = pointOne;
+    this.pointTwo = pointTwo;
   }
 
   private parseLineString(): Point[] {
     return this._lineString.split(' -> ').map((pointString) => new Point(pointString));
   }
 
+  private getHorizontalPoints(): { x: number; y: number }[] {
+    const y = this.pointOne.y;
+
+    return range(this.pointOne.x, this.pointTwo.x + 1).map((x) => ({ x, y }));
+  }
+
+  private getVerticalPoints(): { x: number; y: number }[] {
+    const x = this.pointOne.x;
+
+    return range(this.pointOne.y, this.pointTwo.y + 1).map((y) => ({ x, y }));
+  }
+
   get isVertical(): boolean {
-    return this._pointOne.x === this._pointTwo.x;
+    return this.pointOne.x === this.pointTwo.x;
   }
 
   get isHorizontal(): boolean {
-    return this._pointOne.y === this._pointTwo.y;
+    return this.pointOne.y === this.pointTwo.y;
+  }
+
+  get points() {
+    return this.isHorizontal ? this.getHorizontalPoints() : this.getVerticalPoints();
   }
 }
