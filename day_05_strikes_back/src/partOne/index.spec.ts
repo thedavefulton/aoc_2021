66e671be5776b7929f3dd7ce6dcc972eb6f46f78
David Fulton <thedavefulton@hey.com>
2021-12-07 21:05:59 -0600

    day_05_strikes_back part 1 complete

--- day_05_strikes_back/src/partOne/index.spec.ts
+++ day_05_strikes_back/src/partOne/index.spec.ts
@@ -1,5 +1,53 @@
-import { sum } from "./index";
+import {
+  allLinePoints,
+  convertInputToArray,
+  isVerticalOrHorizontal,
+  orderPoints,
+  pointToString,
+  range,
+} from "./index";
 
-test("sum", () => {
-  expect(sum()).toBe(0);
+test("convertInputToArray", () => {
+  expect(convertInputToArray("123,456 -> 987,654")).toStrictEqual([
+    123, 456, 987, 654,
+  ]);
+});
+
+test("isVerticalOrHorizontal", () => {
+  expect(isVerticalOrHorizontal([0, 0, 0, 1])).toBe(true);
+  expect(isVerticalOrHorizontal([0, 0, 1, 0])).toBe(true);
+  expect(isVerticalOrHorizontal([0, 1, 0, 0])).toBe(true);
+  expect(isVerticalOrHorizontal([1, 0, 0, 0])).toBe(true);
+
+  expect(isVerticalOrHorizontal([1, 0, 0, 1])).toBe(false);
+  expect(isVerticalOrHorizontal([0, 1, 1, 0])).toBe(false);
+});
+
+test("orderPoints", () => {
+  expect(orderPoints([0, 0, 1, 0])).toStrictEqual([0, 0, 1, 0]);
+  expect(orderPoints([1, 0, 0, 0])).toStrictEqual([0, 0, 1, 0]);
+
+  expect(orderPoints([0, 0, 0, 1])).toStrictEqual([0, 0, 0, 1]);
+  expect(orderPoints([0, 1, 0, 0])).toStrictEqual([0, 0, 0, 1]);
+});
+
+test("range", () => {
+  expect(range(0, 2)).toStrictEqual([0, 1, 2]);
+});
+
+test("allLinePoints", () => {
+  expect(allLinePoints([0, 0, 2, 0])).toStrictEqual([
+    [0, 0],
+    [1, 0],
+    [2, 0],
+  ]);
+  expect(allLinePoints([0, 0, 0, 2])).toStrictEqual([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
+});
+
+test("pointToString", () => {
+  expect(pointToString([0, 0])).toBe("0:0");
 });
--- day_05_strikes_back/src/partOne/index.ts
+++ day_05_strikes_back/src/partOne/index.ts
@@ -1,9 +1,72 @@
 import { readFile, shout } from "../utils";
 
-export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
+export function convertInputToArray(input: string) {
+  return input
+    .split(" -> ")
+    .map((point) => {
+      return point.split(",").map((part) => parseInt(part));
+    })
+    .reduce((acc, cur) => [...acc, ...cur], []);
+}
+
+export function isVerticalOrHorizontal([x1, y1, x2, y2]: number[]) {
+  return x1 === x2 || y1 === y2;
+}
+
+export function orderPoints([x1, y1, x2, y2]: number[]) {
+  if (x1 === x2) {
+    return y2 > y1 ? [x1, y1, x2, y2] : [x2, y2, x1, y1];
+  } else {
+    return x2 > x1 ? [x1, y1, x2, y2] : [x2, y2, x1, y1];
+  }
+}
+
+export function range(a: number, b: number) {
+  if (a > b) throw new Error(`Bad range inputs: [${a}, ${b}]`);
+
+  const arr = [];
+  for (let i = a; i <= b; i++) {
+    arr.push(i);
+  }
+
+  return arr;
+}
+
+export function allLinePoints([x1, y1, x2, y2]: number[]) {
+  return x1 === x2
+    ? range(y1, y2).map((y) => [x1, y])
+    : range(x1, x2).map((x) => [x, y1]);
+}
+
+export function pointToString([x, y]: number[]) {
+  return `${x}:${y}`;
+}
 
 export const partOneShout = async () => {
-  // const lines = await readFile();
+  const lines = await readFile();
 
-  shout("Hello from Part One!");
+  const points = lines
+    .map(convertInputToArray)
+    .filter(isVerticalOrHorizontal)
+    .map(orderPoints)
+    .map(allLinePoints)
+    .reduce((acc, cur) => {
+      acc = [...acc, ...cur];
+
+      return acc;
+    }, [])
+    .map(pointToString)
+    .reduce((acc, cur) => {
+      if (acc[cur]) {
+        acc[cur] += 1;
+      } else {
+        acc[cur] = 1;
+      }
+
+      return acc;
+    }, {} as { [key: string]: number });
+
+  const overlap = Object.entries(points).filter(([, val]) => val > 1).length;
+
+  shout(JSON.stringify(overlap, null, 2));
 };
